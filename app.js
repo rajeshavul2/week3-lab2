f06e9b846652b3c19597bc0d2393128d1073dc34
rajeshavul2 <rajeshavul2@gmail.com>
2023-10-26 17:09:25 +0530

    Solved week3-lab2

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "My first Page" });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { title: "Contact US" });
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
